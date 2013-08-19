f716deae79193bd4cdeaf4442e2366b36245c217
John Feminella <jxf@jxf.me>
2013-08-19 13:37:06 -0400

    Configures Compass initializer for use with project

--- lib/compass_initializer.rb
+++ lib/compass_initializer.rb
@@ -3,15 +3,21 @@ module CompassInitializer
     require 'sass/plugin/rack'
 
     Compass.configuration do |config|
-      config.project_path = Padrino.root
-      config.project_type = :stand_alone
-      config.output_style    = :compressed
+      config.project_path         = Padrino.root
+      config.project_type         = :stand_alone
+      config.output_style         = :compressed
 
-      config.http_path       = "/"
-      config.sass_dir        = "app/assets/stylesheets"
-      config.images_dir      = "app/assets/images"
-      config.css_dir         = "public/stylesheets"
-      config.javascripts_dir = "public/javascripts"
+      config.http_path            = "/"
+      config.sass_dir             = "app/assets/stylesheets"
+      config.images_dir           = "app/assets/images"
+      config.http_images_path     = "/images"
+      config.css_dir              = "public/stylesheets"
+      config.javascripts_dir      = "public/javascripts"
+
+      config.on_stylesheet_error do |f, m|
+        puts "---- error in #{f}"
+        puts m
+      end
     end
 
     Compass.configure_sass_plugin!
