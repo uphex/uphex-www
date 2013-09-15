161ce5bfa02748dd96c34e7783c2a717187fde08
John Feminella <jxf@jxf.me>
2013-09-15 08:06:26 -0400

    Adds placeholder for health check

--- app/controllers/page.rb
+++ app/controllers/page.rb
@@ -1,6 +1,11 @@
 UpHex::Web.controllers do
   helpers ConditionalCommentsHelper
 
+  get "/CloudHealthCheck" do
+    status 200
+    body ''
+  end
+
   get "/:id" do
     render "pages/#{params[:id]}"
   end
