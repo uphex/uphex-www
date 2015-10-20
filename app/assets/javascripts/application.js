5b90e0113bae648cf7f6ba9abebce07802d6de05
Philip Karpiak <philip@eswat.ca>
2015-10-20 09:30:09 -0400

    Prevent scrolling when modal is open

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,11 @@
 var prelaunchBtns = document.getElementsByClassName('js-prelaunch-modal');
 var modal = document.getElementById('modal');
+var body = document.getElementsByTagName('body')[0];
 
 for (var i = 0; i < prelaunchBtns.length; i++) {
   prelaunchBtns[i].addEventListener('click', function(e) {
     e.preventDefault();
+    body.classList.toggle('frozen');
     modal.classList.toggle('hidden');
   }, false)
 }
