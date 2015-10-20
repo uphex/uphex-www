216ef6458924219ce98dbf35a511a6027cea639b
Philip Karpiak <philip@eswat.ca>
2015-10-20 10:11:25 -0400

    Tweak modal overlay display, close modal on overlay click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 var prelaunchBtns = document.getElementsByClassName('js-prelaunch-modal');
 var modal = document.getElementById('modal');
+var overlay = document.getElementById('overlay');
 var body = document.getElementsByTagName('body')[0];
 
 for (var i = 0; i < prelaunchBtns.length; i++) {
@@ -9,3 +10,10 @@ for (var i = 0; i < prelaunchBtns.length; i++) {
     modal.classList.toggle('hidden');
   }, false)
 }
+
+overlay.addEventListener('click', function(e) {
+  if (e.target.id == 'overlay') {
+    body.classList.toggle('frozen');
+    modal.classList.toggle('hidden');
+  }
+}, false)
