bb428540c869757ca28c867be393f435315c419e
Philip Karpiak <philip@eswat.ca>
2015-10-20 10:13:16 -0400

    Make modal toggle into a function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,14 +6,17 @@ var body = document.getElementsByTagName('body')[0];
 for (var i = 0; i < prelaunchBtns.length; i++) {
   prelaunchBtns[i].addEventListener('click', function(e) {
     e.preventDefault();
-    body.classList.toggle('frozen');
-    modal.classList.toggle('hidden');
+    toggleModal();
   }, false)
 }
 
 overlay.addEventListener('click', function(e) {
   if (e.target.id == 'overlay') {
-    body.classList.toggle('frozen');
-    modal.classList.toggle('hidden');
+    toggleModal();
   }
 }, false)
+
+function toggleModal() {
+  body.classList.toggle('frozen');
+  modal.classList.toggle('hidden');
+}
