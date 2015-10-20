5dfb6f5ac3df30141265b972b1cf9973f0ad3dbf
Philip Karpiak <philip@eswat.ca>
2015-10-20 15:37:47 -0400

    Adjust hidden class, add stub for signedup content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
 var prelaunchBtns = document.getElementsByClassName('js-prelaunch-modal');
 var overlay = document.getElementById('overlay');
 var modal = document.getElementById('modal');
-var content = document.getElementById('modal-content');
+var signupContent = document.getElementById('signup-content');
+var signedupContent = document.getElementById('signedup-content');
 var body = document.getElementsByTagName('body')[0];
 
 for (var i = 0; i < prelaunchBtns.length; i++) {
@@ -19,11 +20,13 @@ overlay.addEventListener('click', function(e) {
 
 function toggleModal() {
   body.classList.toggle('frozen');
-  modal.classList.toggle('hidden');
+  modal.classList.toggle('invisible');
   modal.classList.toggle('fade-in');
 }
 
 document.signup.addEventListener('submit', function(e) {
   e.preventDefault();
+  signupContent.classList.toggle('hidden');
+  signedupContent.classList.toggle('hidden');
   //customer io stuff goes here
 })
